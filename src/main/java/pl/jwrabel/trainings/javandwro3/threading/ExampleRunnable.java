43afbfda48216621820ac9f5aa94f07c3b0d6941
Jakub Wrąbel <wrabel.jakub@gmail.com>
2017-05-17 20:57:33 +0200

    init

---
+++ src/main/java/pl/jwrabel/trainings/javandwro3/threading/ExampleRunnable.java
@@ -0,0 +1,20 @@
+package pl.jwrabel.trainings.javandwro3.threading;
+
+/**
+ * Created by jakubwrabel on 17/05/2017.
+ */
+public class ExampleRunnable implements Runnable {
+	@Override
+	public void run() {
+		for (int i = 0; i < 10; i++) {
+
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+
+			System.out.println("Example Thread " + i);
+		}
+	}
+}
