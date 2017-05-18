2a53b0fd60e23e15a16bdd35bef4fac4d6580f84
Jakub Wrąbel <wrabel.jakub@gmail.com>
2017-05-18 19:57:04 +0200

    init

--- src/main/java/pl/jwrabel/trainings/javandwro3/threading/MinThread.java
+++ src/main/java/pl/jwrabel/trainings/javandwro3/threading/MinThread.java
@@ -4,16 +4,31 @@ package pl.jwrabel.trainings.javandwro3.threading;
  * Created by jakubwrabel on 18/05/2017.
  */
 public class MinThread extends Thread {
-	public MinThread(int[] data, int start, int stop) {
+	private int[] data;
+	private int start;
+	private int stop;
+	private int foundMin;
 
+
+	public MinThread(int[] data, int start, int stop) {
+		this.data = data;
+		this.start = start;
+		this.stop = stop;
 	}
 
 	@Override
 	public void run() {
+		int min = data[start];
+		for (int i = start; i <= stop; i++) {
+			if (data[i] < min) {
+				min = data[i];
+			}
+		}
+		this.foundMin = min;
 
 	}
 
-	int getMin(){
-		return  0;
+	int getMin() {
+		return foundMin;
 	}
 }
