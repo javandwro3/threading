30bfd6d9cf612e978d2c18cb0f5cf18691842c11
Jakub Wrąbel <wrabel.jakub@gmail.com>
2017-05-18 19:30:06 +0200

    init

--- src/main/java/pl/jwrabel/trainings/javandwro3/threading/ThreadingArrayMin.java
+++ src/main/java/pl/jwrabel/trainings/javandwro3/threading/ThreadingArrayMin.java
@@ -14,6 +14,59 @@ public class ThreadingArrayMin {
 			data[i] = random.nextInt();
 		}
 
+		// WYSZUKANIE MINUMUM W TABLICY
+//		int min = data[0];
+//		for (int i = 0; i < data.length; i++) {
+//			if(data[i] < min){
+//				min = data[i];
+//			}
+//		}
+
+		int[] mins = new int[2];
+
+		Thread thread1 = new Thread() {
+			@Override
+			public void run() {
+				int min = data[0];
+				for (int i = 0; i < data.length/2; i++) {
+					if (data[i] < min) {
+						min = data[i];
+					}
+				}
+				mins[0] = min;
+			}
+		};
+		thread1.start();
+
+		Thread thread2 = new Thread() {
+			@Override
+			public void run() {
+				int min = data[data.length/2];
+				for (int i = data.length/2; i < data.length; i++) {
+					if (data[i] < min) {
+						min = data[i];
+					}
+				}
+				mins[1] = min;
+			}
+		};
+		thread2.start();
+
+		thread1.join();
+		thread2.join();
+
+		int min = mins[0] < mins[1] ? mins[0] : mins[1];
+
+		// TO SAMO CO
+//		int min2 = 0;
+//		if(mins[0] < mins[1]){
+//			min2 = mins[0];
+//		} else {
+//			min2 = mins[1];
+//		}
+
+		System.out.println("Minimalna wartość w tablicy: " + min);
+
 
 	}
 }
